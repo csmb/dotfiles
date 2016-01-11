87a58e3af88dcf1c878d66e908c20dad24655b11
Christopher Bunting <christophersbunting@gmail.com>
2016-01-10 16:48:59 -0800

    Enable Reminders debug

--- homebrew_install.sh
+++ homebrew_install.sh
@@ -13,7 +13,8 @@ echo "**************************************************************************
 echo "Installng packages:"
 echo "***************************************************************************************************"
 
-brew install brew-cask geoip libevent mongodb node phantomjs rabbitmq readline ruby-build wget autoconf elasticsearch git memcached mysql openssl pkg-config rbenv redis htop-osx ffmpeg cowsay
+brew install brew-cask geoip libevent mongodb node phantomjs rabbitmq readline ruby-build wget autoconf \
+elasticsearch git memcached mysql openssl pkg-config rbenv redis htop-osx ffmpeg cowsay
 
 echo "***************************************************************************************************"
 echo "Complete!"
--- preferences.sh
+++ preferences.sh
@@ -1,8 +1,8 @@
-# Hush Last login: Thu Apr 23 10:14:23 on ttys004
+# Hush Last login
 touch ~/.hushlogin
 
 # Turn off spotlight
 sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.metadata.mds.plist
 
-# Turn on spotlight
-# sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.metadata.mds.plist
+# Enable the debug menu in Reminders (for manual refreshing)
+defaults write com.apple.reminders RemindersDebugMenu -boolean true
