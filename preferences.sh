b7d45a8316638a239f911fc2fdba32d3a03516dc
Christopher Bunting <christophersbunting@gmail.com>
2016-03-04 09:23:01 -0800

    sane history

--- preferences.sh
+++ preferences.sh
@@ -6,3 +6,27 @@ sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.metadata.mds.pl
 
 # Enable the debug menu in Reminders (for manual refreshing)
 defaults write com.apple.reminders RemindersDebugMenu -boolean true
+
+## SANE HISTORY DEFAULTS via https://github.com/mrzool/bash-sensible ##
+
+# Append to the history file, don't overwrite it
+shopt -s histappend
+
+# Save multi-line commands as one command
+shopt -s cmdhist
+
+# Record each line as it gets issued
+PROMPT_COMMAND='history -a'
+
+# Huge history. Doesn't appear to slow things down, so why not?
+HISTSIZE=500000
+HISTFILESIZE=100000
+
+# Avoid duplicate entries
+HISTCONTROL="erasedups:ignoreboth"
+
+# Don't record some commands
+export HISTIGNORE="&:[ ]*:exit:ls:bg:fg:history:clear"
+
+# Useful timestamp format
+HISTTIMEFORMAT='%F %T '
